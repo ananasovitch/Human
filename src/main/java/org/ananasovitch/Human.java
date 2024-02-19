c4506eb4ee257022e224d9e829db4af464e4d6a8
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-02-19 18:32:13 +0300

    commit

---
+++ src/main/java/org/ananasovitch/Human.java
@@ -0,0 +1,15 @@
+package org.ananasovitch;
+
+
+import lombok.*;
+
+@ToString
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Human {
+    String firstName;
+    String secondName;
+}
---
+++ src/main/java/org/ananasovitch/Main.java
@@ -0,0 +1,8 @@
+package org.ananasovitch;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human);
+    }
+}
